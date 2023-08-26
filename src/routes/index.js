1131f6ce05292b085f26c253408503fc6613cbc7
paula cano <pcanocerquera@gmail.com>
2023-08-25 19:48:30 -0500

    cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from "express"
 const router = Router()
 
-router.get('/',(req, res) => res.render('index',{ title: 'este es el index'}))
+router.get('/',(req, res) => res.render('index',{ title: 'Inicio'}))
 router.get('/servicios',(req, res) => res.render('servicios', {title: 'Servicios'}))
 router.get('/testimonios',(req, res) => res.render('testimonios', {title: 'Acerca de nosotros'}))
 router.get('/contact',(req, res) => res.render('contact', {title: 'formulario contacto'}))
